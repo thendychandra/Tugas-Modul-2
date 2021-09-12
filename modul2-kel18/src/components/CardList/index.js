3bd1336c05f5c59885c35fb8c438f57200925ce3
thendychandra <thendychandra@gmail.com>
2021-09-12 22:28:50 +0700

    Tugas Modul 2

--- modul2-kel18/src/components/CardList/index.js
+++ modul2-kel18/src/components/CardList/index.js
@@ -7,11 +7,11 @@ export default function CardList(props) {
         <h2 className="text-2x1 font-extrabold tracking-tight text-gray-900">
           Daftar Praktikan RPLBK 2021
         </h2>
-        <div class="grid grid-cols-1 gap-6 xl:grid-cols-3 sm:grid-cols-1 lg:grid-cols-3">
+        <div class="grid grid-cols-1 gap-6 xl:grid-cols-3 sm:grid-cols-2 lg:grid-cols-3">
           {contacts.map((contact, index) => (
             <div className="mt-6 grid">
               <div className="group relative shadow">
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 sm:h-80 lg:aspect-none">
                   <img
                     src={contact.avatar}
                     className="w-full h-full object-center object-cover lg:w-full lg:h-full"
